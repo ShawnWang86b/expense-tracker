ac03560654ec5005bc61ad74a85168d7b74d50a6
Shawn <shawn.wang86b@gmail.com>
2024-06-24 22:36:16 +1000

    feat: add sign up and log in resolver

--- backend/resolvers/user.resolver.js
+++ backend/resolvers/user.resolver.js
@@ -1,15 +1,70 @@
 import { users } from "../dummyData/data.js";
+import bcrypt from "bcryptjs";
 
 const userResolver = {
   Query: {
-    users: () => {
+    users: (_, _, { req, res }) => {
       return users;
     },
     user: (_, { userId }) => {
       return users.find((user) => user._id === userId);
     },
   },
-  Mutation: {},
+  Mutation: {
+    signUp: async (_, { input }, context) => {
+      try {
+        const { username, name, password, gender } = input;
+        if (!username || !name || !password || !gender) {
+          throw new Error("All fields are required");
+        }
+
+        // First step: check whether username existed
+        const existingUser = await User.findOne({ username });
+        if (existingUser) {
+          throw new Error("User already exists");
+        }
+
+        // Second step: Hash
+        const salt = await bcrypt.genSalt(10);
+        // Hash this password with this salt
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+        const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+        const newUser = new User({
+          username,
+          name,
+          password: hashedPassword,
+          gender,
+          profilePicture: gender === "male" ? boyProfilePic : girlProfilePic,
+        });
+
+        await newUser.save();
+        await context.login(newUser);
+        return newUser;
+      } catch (error) {
+        console.error("Error in signUp: ", err);
+        throw new Error(err.message || "Internal server error");
+      }
+    },
+    login: async (_, { input }, context) => {
+      try {
+        const { username, password } = input;
+        if (!username || !password) throw new Error("All fields are required");
+        const { user } = await context.authenticate("graphql-local", {
+          username,
+          password,
+        });
+
+        await context.login(user);
+        return user;
+      } catch (err) {
+        console.error("Error in login:", err);
+        throw new Error(err.message || "Internal server error");
+      }
+    },
+  },
 };
 
 export default userResolver;
